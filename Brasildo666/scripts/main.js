44e70dc450c52fbafb655254c55749f057013a79
ViktorClipel <v1kt0rcl1p8l@gmail.com>
2024-05-13 22:28:07 -0300

    Add files via upload

---
+++ Brasildo666/scripts/main.js
@@ -0,0 +1,18 @@
+let CADASTRO = document.querySelector(".cadastro");
+
+CADASTRO.onclick = () => {
+        const cadastro_input_e_mail = prompt("digite seu e-mail");
+        const cadastro_input_senha = prompt("digite sua senha");
+};
+
+let ENTRAR = document.querySelector(".entrar");
+
+ENTRAR.onclick = () => {
+        const entrar_input_email = prompt("digite seu e-mail");
+};
+ 
+const dio_clash = document.querySelector(".dio-clash");
+
+function teste() {
+        alert("")
+}
